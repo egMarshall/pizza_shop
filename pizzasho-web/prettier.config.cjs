164d88f43c047c1223327608a837c117bba0e329
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-28 19:15:28 -0300

    initial commit.

---
+++ pizzasho-web/prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "printWidth": 120,
+  "bracketSpacing": true
+}
