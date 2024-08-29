8b52d5dabbb96aedcebfedae00dae32587b5512a
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-28 23:28:33 -0300

    [FEAT] Ambient setup with routes configuration using `react-router` and SEO title page with `React Helmet Async`.

--- pizzasho-web/src/App.tsx
+++ pizzasho-web/src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from './components/ui/button';
+import { RouterProvider } from 'react-router-dom';
 import './global.css';
+
+import { router } from './routes';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   );
 }
