850a870065478d7b7f03ab12d73f5730f8b1bbe5
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-31 00:28:09 -0300

    [FEAT] Adds `get-managed-restaurant`, `get-orders`, `get-profile`, `sign-out`. routes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,4 +3,13 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 });
+
+// if (env.VITE_ENABLE_API_DELAY) {
+//   api.interceptors.request.use(async (config) => {
+//     await new Promise((response) => setTimeout(response, 2000));
+
+//     return config;
+//   });
+// }
