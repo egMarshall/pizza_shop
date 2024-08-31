5cc900e94e80f3fb75c5e5d98bac4a760710923b
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-31 00:29:52 -0300

    [FEAT] Implements api call to `account-menu`.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,31 +8,78 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from './ui/dropdown-menu';
+import { getProfile } from '@/api/get-profile';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { getManagedRestaurant } from '@/api/get-managed-restaurant';
+import { Skeleton } from './ui/skeleton';
+import { Dialog, DialogTrigger } from './ui/dialog';
+import { StoreProfileDialog } from './store-profile-dialog';
+import { signOut } from '@/api/sign-out';
+import { useNavigate } from 'react-router-dom';
 
 export function AccountMenu() {
+  const navigate = useNavigate();
+
+  const { data: profile, isLoading: isProfileLoading } = useQuery({
+    queryKey: ['userProfile'],
+    queryFn: getProfile,
+    staleTime: Infinity,
+  });
+
+  const { data: managedRestaurant, isLoading: isRestaurantLoading } = useQuery({
+    queryKey: ['restaurant'],
+    queryFn: getManagedRestaurant,
+    staleTime: Infinity,
+  });
+
+  const { mutateAsync: signOutFn, isPending: isSignOutPending } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true });
+    },
+  });
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex select-none items-center gap-2">
-          Pizza Shop
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          <span>Boogie Jones</span>
-          <span className="text-xs font-normal text-muted-foreground">boogie@email.com</span>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da Loja</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" className="flex select-none items-center gap-2">
+            {isRestaurantLoading ? <Skeleton className="h-4 w-40" /> : managedRestaurant?.name}
+
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isProfileLoading ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-4 w-40" />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-xs font-normal text-muted-foreground">{profile?.email}</span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da Loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400" asChild disabled={isSignOutPending}>
+            <button className="w-full" onClick={() => signOutFn}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   );
 }
