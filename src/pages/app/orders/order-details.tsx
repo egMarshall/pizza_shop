7ed90f5b2856fed7db1b4d37963591c2f2910c9f
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-14 20:03:50 -0300

    [FEAT] Adds skeleton when order details is loading.

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -5,6 +5,7 @@ import { Table, TableBody, TableCell, TableHead, TableRow, TableHeader, TableFoo
 import { useQuery } from '@tanstack/react-query';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import { OrderDetailsSkeleton } from './order-details-skeleton';
 
 export interface OrderDetailsProps {
   orderId: string;
@@ -25,7 +26,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do Pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -106,6 +107,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   );
