b706c2f30a5f7a8e5c465b5867d66cd6e9f442f6
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-15 01:23:01 -0300

    [TEST] Create test setup.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { InlineConfig } from 'vitest/node';
+import { UserConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
