a25f80562679ce15f34f264dfc294236623b9b23
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-17 11:19:31 -0300

    [CHORE] Configure MSW.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import { enableMocks } from '@/api/mocks';
+
+enableMocks().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
