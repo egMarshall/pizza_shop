d71c437246101de8cd5f5f176133b8387e710acc
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-17 10:52:58 -0300

    [TEST] Adds verification to default email input value on search params in `sign-in`.

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { SignIn } from './sign-in';
+import { MemoryRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/react-query';
+import { HelmetProvider } from 'react-helmet-async';
+
+describe('SignIn', () => {
+  it('Should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter initialEntries={['/sign-in?email=johndoe@email.com']}>
+              <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        );
+      },
+    });
+
+    const emailInput = wrapper.getByLabelText('Seu e-mail') as HTMLInputElement;
+
+    expect(emailInput.value).toEqual('johndoe@email.com');
+  });
+});
